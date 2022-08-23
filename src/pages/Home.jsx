c1d1b6c9aafa93a022226c2106179fe56cadd219
Bijay Thakur <bijaythakur330@gmail.com>
2022-08-23 13:41:51 +0545

    userlist component added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
+import UserResults from '../components/users/UserResults'
+
 function Home() {
   return (
-    <div>
-      <h1 className='text-6xl'>Welcome</h1>
-      {process.env.REACT_APP_GITHUB_TOKEN}
-    </div>
+    <>
+      {/* SEARCH COMPONENT */}
+      <UserResults />
+    </>
   )
 }
 export default Home
